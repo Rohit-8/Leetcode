eded5c87d8da7305409be2af921df5209c1303c3
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-06-29 19:07:36 +0530

    Time: 188 ms (49.80%), Space: 12.6 MB (31.79%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -4,6 +4,7 @@ public:
         if(a[0]==b[0]) return a[1]<b[1];
         return a[0]>b[0];
     }
+    
     vector<vector<int>> reconstructQueue(vector<vector<int>>& p) {
         
         sort(p.begin(),p.end(),comp);
