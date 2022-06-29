2308871228a12f0bbaf7b3e8b40ab290ec7b8bd1
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-06-29 12:35:35 +0530

    Time: 380 ms (13.87%), Space: 107.5 MB (5.05%) - LeetHub

---
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& arr) {
+        
+        long long ans = 0;
+        vector<long long> r,l;
+        stack<long long> s;
+        int n = arr.size();
+        for(long long i = 0; i< n; i++){
+            
+            while(!s.empty() and arr[s.top()] >= arr[i]) s.pop();
+            
+            if(s.empty()) l.push_back(-1);
+            else l.push_back(s.top());
+            s.push(i);
+        }
+        while(!s.empty()) s.pop();
+        
+        // for(int i = 0; i< n;i++) cout<<l[i]<<" ";
+        // cout<<endl;
+        for(long long i = n-1; i>=0; i--){
+            
+            while(!s.empty() and arr[s.top()] >= arr[i]) s.pop();
+            
+            if(s.empty()) r.push_back(n);
+            else r.push_back(s.top());
+            s.push(i);
+        }
+        reverse(r.begin(), r.end());
+        // for(int i = 0; i< n;i++) cout<<r[i]<<" ";
+        // cout<<endl;
+        
+        for(long long i = 0; i< n; i++){
+            
+            ans = max(ans, (r[i] - l[i] -1)*arr[i]);
+            
+        }
+        
+        return ans;
+    }
+};
