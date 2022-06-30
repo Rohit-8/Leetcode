f3c6392eaf7a6b758a0585fd83f83fe8edbab2cd
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-07-01 00:30:37 +0530

    Time: 17 ms (49.26%), Space: 10.9 MB (59.74%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -3,10 +3,10 @@ public:
     int minMoves2(vector<int>& a) {
         
         sort(a.begin(), a.end());
-        int sum = a[a.size()/2];
+        // int sum = a[a.size()/2];
         int ans = 0;
         for(int i = 0; i< a.size(); i++)
-            ans += abs(sum-a[i]);
+            ans += abs(a[a.size()/2]-a[i]);
         
         return ans;
     }
