8ffb08ea047db57e93441b44a1f28aa8fcc21054
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-07-08 13:56:06 +0530

    Time: 24 ms (87.86%), Space: 11.3 MB (99.12%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -9,17 +9,17 @@ public:
         for(int i = 0; i < n; i++){
             
             int mn = 10000000, k = 0;
-            for(int j = 0; j < m; j++){
+            for(int j = 0; j < m; j++){ // in this loop i am finding the minimum element in row
                 if(mn > a[i][j])
                 mn = min(mn, a[i][j]), k = j;
             }
             int mx = 0, x = 0;
-            for(int j = 0; j < n; j++){
+            for(int j = 0; j < n; j++){ // in this loop i am finding the max element in that column that has min ele. in row
                 if(mx < a[j][k])
                     mx = max(a[j][k], mx), x = j;
             }
             
-            if(x == i)
+            if(mx == mn) // if both element matches then it is our ans
                 ans.push_back(a[i][k]);
         }
         
