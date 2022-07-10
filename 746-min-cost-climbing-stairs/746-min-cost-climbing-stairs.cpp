76f3ee69ccd98dcca5e22ff083e9d7c5f58ea553
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-07-10 22:31:04 +0530

    Time: 24 ms (6.67%), Space: 13.7 MB (51.95%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -9,6 +9,7 @@ public:
         for(int i=2;i<cost.size();i++)
             dp[i]=cost[i]+ min(dp[i-1], dp[i-2]);
         
+        
         return min(dp[cost.size()-1], dp[cost.size()-2]);
     }
 };
