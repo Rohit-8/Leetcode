dff23ec97849e7d5aac100f6c151557cd7743b13
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-07-10 22:30:51 +0530

    Time: 9 ms (61.41%), Space: 13.9 MB (28.79%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -5,6 +5,7 @@ public:
         vector<int> dp(cost.size());
         dp[0]=cost[0];
         dp[1]=cost[1];
+        
         for(int i=2;i<cost.size();i++)
             dp[i]=cost[i]+ min(dp[i-1], dp[i-2]);
         
