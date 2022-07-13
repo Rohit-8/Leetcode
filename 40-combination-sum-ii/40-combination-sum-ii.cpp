91f41c3ab0c961f9f568cd9b0164e201362bc9e9
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-07-13 23:48:59 +0530

    Time: 14 ms (36.01%), Space: 11.1 MB (33.37%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    
+    vector<vector<int>> ans;
+    set<vector<int>> st;
+    void res(vector<int> &a, int t, int n, vector<int> &v, int &sum, int i){
+        
+        if(sum == t){
+            st.insert(v);
+            return;
+        }
+        if(i >= n) return;
+        
+        for( ; i < n; i ++){
+            
+            if(sum + a[i] <= t){
+                v.push_back(a[i]); 
+                sum = sum + a[i];
+                res(a, t, n, v, sum, i + 1);
+                v.pop_back();
+                sum -= a[i];
+                
+                while(i+1 < n and a[i+1] == a[i]) i++;
+            }
+            else return;
+        }
+        return;
+    }
+    
+    vector<vector<int>> combinationSum2(vector<int>& a, int t) {
+        
+        vector<int> v;
+        sort(a.begin(), a.end());
+        int sum = 0;
+        res(a, t, a.size(), v, sum, 0);
+        
+        for(auto x: st)
+            ans.push_back(x);
+        
+        return ans;
+    }
+};
