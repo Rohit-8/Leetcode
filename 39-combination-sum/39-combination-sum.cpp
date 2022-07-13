55ebf5186c8e1bf7ba5fd3b1d2900a0f6870bac6
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-07-13 22:54:53 +0530

    Time: 19 ms (44.59%), Space: 15.7 MB (43.16%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    vector<vector<int>> ans;
+    void res(vector<int> &a, int t, int n, vector<int> v, int sum, int i){
+        if(i >= n) return ;
+        if(sum == t){
+            ans.push_back(v);
+            return;
+        }
+        
+        for( ; i < n; i ++){
+            if(sum + a[i] <= t){
+                v.push_back(a[i]);  
+                res(a, t, n, v, sum + a[i], i);
+                v.pop_back();
+            }
+        }
+        
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& a, int t) {
+        vector<int> v;
+        res(a, t, a.size(), v, 0, 0);
+        
+        return ans;
+    }
+};
