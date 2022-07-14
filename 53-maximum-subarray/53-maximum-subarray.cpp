d4756add88ce2fbe51b505039143450432328ad9
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-07-14 11:41:21 +0530

    Time: 220 ms (15.00%), Space: 67.8 MB (53.44%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -3,6 +3,7 @@ public:
     int maxSubArray(vector<int>& a) {
         int sum = 0;
         int ans = INT_MIN;
+        
         for(int i = 0; i < a.size(); i++){
             sum = max(sum + a[i], a[i]);
             ans = max(ans, sum);
