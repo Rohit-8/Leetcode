7f7874465694c951815927f580b7476fc67f4e49
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-07-16 23:28:26 +0530

    Time: 26 ms (28.99%), Space: 15.9 MB (51.28%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -6,6 +6,7 @@ public:
         int num1 = -1, num2 = -1, c1 = 0, c2 = 0;
         
         for(int i = 0; i < n; i++){
+            
             if(num1 == a[i]) c1 ++;
             else if(num2 == a[i]) c2++;
             else if(c1 == 0){
@@ -15,6 +16,7 @@ public:
             else if(c2 == 0) num2 = a[i], c2 ++;
             else c1--, c2--;
         }
+        
         c1 = c2 = 0;
         for(int i = 0;i < n; i++){
             if(a[i] == num1) c1 ++;
