682012715ea35c0e46d32964f26f3df519ad47c5
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-07-16 23:02:56 +0530

    Time: 0 ms (100.00%), Space: 9 MB (89.69%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    void merge(vector<int>& a, int m, vector<int>& b, int n) {
-        if(n == 0) return;
+    void merge(vector<int>& a1, int n, vector<int>& a2, int m) {
         
-        int j = n-1, k = m-1;
-        int i ;
-        for(i = n+m-1;k>=0 and j>=0;){
-            if(j>=0 and a[k]>=b[j])
-                a[i--] = a[k--];
-            else if(j>=0 and a[k]<b[j])a[i--] = b[j--];
+        for(int i = 0, j = n; i < m; i++, j++){
+            a1[j] = a2[i];
         }
-        while(j>=0){
-            a[i--] = b[j--];
+        
+        int gap = (n + m)/2 + (n+m)%2;
+        
+        while(gap > 0){
+            int i = 0, j = gap;
+            while(j < n + m){
+                if(a1[i] > a1[j])
+                    swap(a1[i], a1[j]);
+                i++, j++;
+            }
+            if(gap == 1) break;
+            gap = gap/2 + gap%2;
         }
-        return;
     }
 };
