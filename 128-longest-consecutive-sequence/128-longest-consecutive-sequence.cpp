7af27022bd63bb160c267829f952e6dee881fa31
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-07-17 00:18:20 +0530

    Time: 825 ms (24.55%), Space: 49.8 MB (50.11%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,56 +1,21 @@
 class Solution {
 public:
-    int parent[100001], Rank[100001];
-    
-    int find(int a){
-        if(parent[a] == a)
-            return a;
-        return parent[a] = find(parent[a]);
-    }
-    
-    void Union(int a, int b){
-        
-        a = find(a);
-        b = find(b);
-        
-        if(a == b) return;
-        
-        if(Rank[a] > Rank[b]){
-            parent[b] = a; 
-            Rank[a] += Rank[b];
-        }
-        
-        else{
-            parent[a] = b;
-            Rank[b] += Rank[a];
-        } 
-    }
-    
-    int longestConsecutive(vector<int>& arr) {
-        int n = arr.size();
-        if(n == 0) return 0;
-        
-        for(int i = 0; i < n; i++) parent[i] = i, Rank[i] = 1;
-        
-        unordered_map<int, int> mp;
-        
-        for(int i = 0; i < n; i++){
-            
-            if(mp.find(arr[i]) != mp.end()) continue;
-            
-            if(mp.find(arr[i] - 1) != mp.end())
-                Union(mp[arr[i] - 1], i);
-            
-            if(mp.find(arr[i] + 1) != mp.end())
-                Union(mp[arr[i] + 1], i);
-            
-             mp[arr[i]] = i;
-        }
+    int longestConsecutive(vector<int>& a) {
+        int n = a.size();
+        unordered_map<int,int> m;
+        for(int i = 0;i < n; i++)
+            m[a[i]]++;
         
         int ans = 0;
-        for(int i = 0; i < n; i++)
-            ans = max(ans, Rank[i]);
-            
-        return ans; 
+        for(int i = 0; i < n; i++){
+            if(m.find(a[i]-1) == m.end()){
+                int c = 1;
+                int x = a[i];
+                while(m.find(++x) != m.end()) c++;
+                ans = max(ans, c);
+            }
+        }
+        
+        return ans;
     }
-}; 
+};
