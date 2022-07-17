aead3b75ba312972c5dcc00b736d931dacf85e77
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-07-17 13:54:58 +0530

    Time: 106 ms (23.87%), Space: 22.2 MB (5.01%) - LeetHub

---
+++ 85-maximal-rectangle/85-maximal-rectangle.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    
+    int largestRectangleArea(vector<int>& arr) {
+        
+        long long ans = 0;
+        vector<long long> r,l;
+        stack<long long> s;
+        int n = arr.size();
+        for(long long i = 0; i< n; i++){
+            
+            while(!s.empty() and arr[s.top()] >= arr[i]) s.pop();
+            
+            if(s.empty()) l.push_back(-1);
+            else l.push_back(s.top());
+            s.push(i);
+        }
+        while(!s.empty()) s.pop();
+        
+        for(long long i = n-1; i>=0; i--){
+            
+            while(!s.empty() and arr[s.top()] >= arr[i]) s.pop();
+            
+            if(s.empty()) r.push_back(n);
+            else r.push_back(s.top());
+            s.push(i);
+        }
+        reverse(r.begin(), r.end());
+        // for(int i = 0; i< n;i++) cout<<r[i]<<" ";
+        // cout<<endl;
+        
+        for(long long i = 0; i< n; i++){
+            
+            ans = max(ans, (r[i] - l[i] -1)*arr[i]);
+            
+        }
+        
+        return ans;
+    }
+    
+    int maximalRectangle(vector<vector<char>>& a) {
+        int n = a.size(), m = a[0].size();
+        vector<vector<int>> h(n, vector<int> (m, 0));
+        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j< m; j++){
+                if(i > 0 and a[i][j] == '1') h[i][j] = h[i-1][j] + 1;
+                else if(a[i][j] == '1' and i == 0) h[i][j] = 1;
+            }
+        
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            int x = largestRectangleArea(h[i]);
+            ans = max(ans, x);
+        }
+        
+        return ans;
+    }
+};
