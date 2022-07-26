3da4f377d529926d7e385acf4e0cbf29e3d7688c
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-07-26 23:15:17 +0530

    Time: 8 ms (30.43%), Space: 11.4 MB (5.30%) - LeetHub

---
+++ 1024-video-stitching/1024-video-stitching.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    vector<vector<int>> dp;
+    int res(vector<vector<int>> &a, int t, int prev, int n){
+        if(prev >= t) return 0;
+        if(n >= a.size()) return 1000000000;
+        
+        if(dp[n][prev] != -1) return dp[n][prev];
+        
+        int l = res(a, t, prev, n+1);
+        if(a[n][0] <= prev){
+            l = min(1 + res(a, t, a[n][1], n+1), l);
+        }
+        
+        return dp[n][prev] = l;
+        
+    }
+    
+    int videoStitching(vector<vector<int>>& a, int t) {
+        sort(begin(a), end(a));
+        
+        dp.resize(101, vector<int> (105, -1));
+        int ans = res(a, t, 0, 0);
+        if(ans >= 1000000000) return -1;
+        
+        return ans;
+    }
+};
