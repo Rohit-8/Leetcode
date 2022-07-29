fa9b4cc941f936b17ef814ed24c16c7d4764c2c3
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-07-30 01:54:26 +0530

    Time: 39 ms (76.57%), Space: 8.7 MB (87.36%) - LeetHub

---
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+    int pow(long long n){
+        if(n == 1) return 0;
+        int c = 0;
+        while(n > 1){
+            c ++;
+            if(n & 1) n = n * 3 + 1;
+            else n /= 2;
+        }
+        
+        return c;
+    }
+    
+    static bool cmp(pair<int,int> &a, pair<int,int> &b){
+        if(a.first != b.first) return a.first < b.first;
+        return a.second < b.second;
+    }
+    
+    int getKth(int lo, int hi, int k) {
+        vector<pair<int,int>> v;
+        
+        for(int i = lo; i <= hi; i++){
+            v.push_back({pow(i*1ll), i});
+        }
+        sort(begin(v), end(v), cmp);
+        
+        return v[k-1].second;
+    }
+};
