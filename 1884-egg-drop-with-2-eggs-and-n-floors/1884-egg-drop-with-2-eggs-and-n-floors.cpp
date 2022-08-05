e841f5d7f0cab75827e52cc2469ad125ebe9420a
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-08-05 21:31:52 +0530

    Time: 307 ms (10.66%), Space: 7.3 MB (16.58%) - LeetHub

---
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    vector<vector<int>> dp;
+    int mcm(int egg, int f){
+        if(f == 1 or f == 0) return f;
+        if(egg == 1) return f;
+        
+        if(dp[egg][f] != -1) return dp[egg][f];
+        
+        int res = INT_MAX;
+        for(int i = 1; i <= f; i++){
+            int t = 1 + max(mcm(egg-1, i-1), mcm(egg, f-i));
+            
+            res = min(t, res);
+        }
+        
+        return dp[egg][f] = res;
+    }
+    
+    int twoEggDrop(int n) {
+        dp.resize(3, vector<int> (n + 1, -1));
+        
+        return mcm(2, n);
+    }
+};
