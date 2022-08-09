5068a2997323347e6575b45b5d1169041017f16b
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-08-09 21:02:20 +0530

    Time: 71 ms (87.08%), Space: 9.2 MB (67.38%) - LeetHub

---
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        int mod=1e9+7;
+        map<int,int>dp;
+        int ans=1;  
+        dp[arr[0]]=1;
+        for(int i=1;i<n;i++){
+            dp[arr[i]]=1;
+                for(int j=0;j<i;j++){
+                    if(arr[i]%arr[j]==0 && dp.find(arr[i]/arr[j])!=dp.end())
+                        dp[arr[i]]=
+                        (
+                            (long int)dp[arr[i]]+
+                            (long int)dp[arr[j]]*
+                            (long int)dp[arr[i]/arr[j]]
+                        )%mod;
+                }
+            ans=((long int)ans+(long int)dp[arr[i]])%mod;
+        }
+        return ans;
+	}
+};
