11b8295436be8eff0680bf2e271592928adf73d6
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-08-17 00:17:44 +0530

    Time: 281 ms (71.30%), Space: 82.7 MB (67.43%) - LeetHub

---
+++ 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> getStrongest(vector<int>& arr, int k) {
+        int n = arr.size();
+        
+        sort(arr.begin(), arr.end());
+        vector<int> ans;
+        // if(n & 1)
+        {
+            int j = 0;
+            int c = 0;
+            int mid = arr[(n-1)/2];
+            for(int i = n-1; i >= j; ){
+                c++;
+                if(abs(arr[i] - mid) >= abs(arr[j] - mid)){
+                    ans.push_back(arr[i]); i--;
+                }
+                else ans.push_back(arr[j]), j++;
+                if(c == k) break;
+            }
+        }
+        
+        sort(ans.begin(), ans.end(), [](int &a, int &b){
+            return a > b;
+        });
+        return ans;
+    }
+};
