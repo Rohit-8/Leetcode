deedc9a0456c8008192b642009d53b7c82d8c554
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-08-18 20:53:22 +0530

    Time: 3 ms (51.01%), Space: 6.1 MB (83.39%) - LeetHub

---
+++ 306-additive-number/306-additive-number.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    bool res(long long l, long long r, string s, bool f){
+        if(s.size() == 0 and f)
+            return 1;
+        
+        string sum = to_string(l+r);
+        int n = min(s.size(), sum.size());
+        if(s.substr(0, n) == sum){
+            return res(r, stol(sum), s.substr(n), true);
+        }
+        return 0;
+    }
+    
+    bool isAdditiveNumber(string a) {
+        for(int i = 1; i < a.size() - 1; i++){
+            
+            long long l = stol(a.substr(0, i));
+            if(to_string(l) != a.substr(0, i)){
+                break;
+            }
+            for(int j = i + 1; j < a.size(); j++){
+                long long r = stol(a.substr(i, j-i));
+                if(to_string(r) != a.substr(i, j-i))
+                    break;
+                if(res(l, r, a.substr(j), false))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
