4e624c7fa309babe155775d3ed83bb9770f4f1b7
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-08-21 22:29:50 +0530

    Time: 27 ms (55.74%), Space: 7.5 MB (68.03%) - LeetHub

---
+++ 936-stamping-the-sequence/936-stamping-the-sequence.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    
+    string st,tar;
+    
+    bool isvalid(int pos){
+        int counh = 0;
+        for(int i = 0; i < st.size(); i++){
+            if(tar[i+pos]=='#')
+                counh++;
+            
+            else if(tar[i + pos] != st[i])
+                return false;
+        }
+        if(counh != st.size()){
+            return true;
+        }
+        return false;
+    }
+    
+    vector<int> movesToStamp(string stamp, string target){
+        st = stamp;
+        tar = target;
+        vector<int> res;
+        int lim = 10 * target.size();
+        int tot = 0;
+        while(res.size() < lim){
+            int pr = res.size();
+            for(int i = 0; i <= tar.size()-st.size(); i++){ 
+                if(isvalid(i)){ 
+                    cout<<i<<" ";
+                    res.push_back(i);
+                    for(int j = 0; j < st.size(); j++){
+                        if(tar[j+i] != '#'){
+                            tot++;
+                            tar[i+j] = '#';
+                        }
+                    }
+                }
+            }
+            if(pr == res.size()){
+                break;
+            }
+        }
+        
+        reverse(res.begin(), res.end());
+        if(tot == tar.size()){
+            return res;
+        }
+        return {};
+    }
+};
