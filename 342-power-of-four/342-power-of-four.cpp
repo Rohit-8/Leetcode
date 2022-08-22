37db813042b2bc56b5f9387e6dedc6121758624b
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-08-22 23:00:45 +0530

    Time: 7 ms (13.19%), Space: 5.7 MB (99.01%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        return n > 0 and !(n&(n-1)) && (n&0x55555555);
+        return n > 0 and !(n&(n-1)) and (n & 0x55555555);
     }
 };
