72a34881473ef56edb82f0a457881ec61fed879b
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-08-24 00:11:49 +0530

    Time: 3 ms (44.62%), Space: 6.2 MB (63.59%) - LeetHub

--- 1033-moving-stones-until-consecutive/1033-moving-stones-until-consecutive.cpp
+++ 1033-moving-stones-until-consecutive/1033-moving-stones-until-consecutive.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     vector<int> numMovesStones(int a, int b, int c) {
-        int mn = min({a, b, c});
-        int mx = max({a, b, c});
-        int y = mx - mn - 2;
-        if(y == 0)
-            return {0, 0};
-        if(abs(a-b) < 3 or abs(b-c) < 3 or abs(c-a) < 3)
-            return {1, y};
-        
-        return {2, y};
+        auto [m, M] = minmax({abs(b-a), abs(c-b), abs(c-a)});
+        return {(m > 2) + (M > m+1), M-2};
     }
 };
