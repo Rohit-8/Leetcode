f7283acdd57e61a15933abc11d2eda4702e02f94
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-08-29 11:06:33 +0530

    Time: 90 ms (76.94%), Space: 52.4 MB (32.12%) - LeetHub

---
+++ 795-number-of-subarrays-with-bounded-maximum/795-number-of-subarrays-with-bounded-maximum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numSubarrayBoundedMax(vector<int>& a, int l, int r) {
+        int n = a.size();
+        int ans = 0;
+        int mx = 0;
+        for(int i = 0, j = 0; i < n and j < n; j++){
+            if(a[j] > r){
+                mx = 0; i = j + 1;
+                continue;
+            }
+            if(a[j] <= r and a[j] >= l){
+                mx = j - i + 1;
+                ans += mx;
+            }
+            else{
+                ans += mx;
+            }
+            
+        }
+        
+        return ans;
+    }
+};
