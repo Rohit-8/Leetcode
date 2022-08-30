a1dbc76a9d5ea55b7e07e356722f44d1a358cb6f
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-08-30 15:12:58 +0530

    Time: 39 ms (50.41%), Space: 13.8 MB (48.18%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,37 +1,49 @@
 class Solution {
 public:
+    vector<int> vis, rank, par;
     
-    deque<int> vis;
-    deque<vector<int>> adj;
+    int findpar(int x){
+        if(par[x] == x) return x;
+        return findpar(par[x]);
+    }
     
-    void dfs(int node){
-        vis[node] = 1;
-        for(auto x: adj[node])
-            if(vis[x] == 0)
-                dfs(x);
+    void unionp(int u, int v){
+        int x = findpar(u);
+        int y = findpar(v);
+        
+        if(rank[x] > rank[y]){
+            par[y] = x;
+        }
+        else if(rank[y] > rank[x])
+            par[x] = y;
+        else{
+            par[x] = y;
+            rank[y]++;
+        }
     }
     
     int findCircleNum(vector<vector<int>>& a) {
         int n = a.size();
-        adj.resize(n + 1, vector<int> (n + 1));
         vis.resize(n + 1, 0);
+        rank.resize(n + 1);
+        par.resize(n + 1);
+        
+        for(int i = 0; i <= n; i++){
+            rank[i] = 0, par[i] = i;
+        }
         
         for(int i = 0; i < n; i++){
-            // int x = i;
             for(int j = 0; j < n; j++){
-                if(a[i][j]){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
+                if(a[i][j])
+                    unionp(i, j);
+            }    
         }
         
         int ans = 0;
-        for(int i = 0; i < n; i++)
-            if(vis[i] == 0){
-                ans++;
-                dfs(i);
-            }
+        for(int i = 0; i < n; i++){
+            // int x = findpar(i);
+            if(par[i] == i) ans++;
+        }
         
         return ans;
     }
