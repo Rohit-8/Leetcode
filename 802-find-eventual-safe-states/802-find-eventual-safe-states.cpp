9110b5d09675d352233c6d7315d609a03d90cd83
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-09-02 00:33:25 +0530

    Time: 466 ms (9.98%), Space: 56.2 MB (31.34%) - LeetHub

---
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    vector<int> vis;
+    map<int,bool> m;
+    bool dfs(vector<vector<int>> &adj, int node, int par){
+        vis[node] = 1;
+        bool f = true;
+        
+        for(auto x: adj[node]){
+            if(vis[x] == 0){
+                if(dfs(adj, x, node) == false)
+                    f = false;
+            }
+            else f = f and m[x];
+        }
+        
+        return m[node] = f;
+    }
+    
+    vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
+        int n = adj.size();
+        vis = vector<int> (n, 0);
+        
+        for(int i = 0; i < n; i++){
+            if(vis[i] == 0)
+                dfs(adj, i, -1);
+        }
+        vector<int> ans;
+        for(int i = 0; i < n; i++)
+            if(m[i] == true)
+                ans.push_back(i);
+        
+        return ans;
+    }
+};
