750c925ab2555331dc72589284b4614c9385e49a
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-09-02 13:15:15 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (55.65%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
-    int solve(int i , int j , vector<vector<int>>&dp , vector<int>& arr)
-    {
-        if(i >= j)
-        {
-            return 0;
+    int mctFromLeafValues(vector<int>& v) {
+         int c=0;
+        stack<int> st;
+        for(int i:v) {
+        while(!st.empty() && st.top() <= i) {
+        int x = st.top();
+        st.pop();
+        c += x * min(st.empty() ? INT_MAX : st.top(),i);
         }
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        int mx = INT_MAX;
-        vector<int> left_max(j - i + 1 , 0) , right_max(j - i + 1 , 0);
-        left_max[0] = arr[i];
-        for(int k = i + 1; k <= j ; k++)
-            left_max[k - i] = max(left_max[k - i  - 1] , arr[k]);
-        
-        right_max[j - i] = arr[j];
-        for(int k = j - 1; k>= i; k--)
-        {
-            right_max[k - i] = max(right_max[k - i + 1], arr[k]);
+        st.push(i);
         }
-        
-        for(int k = i; k < j; k++)
-        {
-            mx = min(mx , solve(i , k , dp , arr) + solve(k + 1 , j , dp , arr) + left_max[k - i]*right_max[k + 1 - i]);
+        int a = st.top();
+        st.pop();
+        while(!st.empty()) {
+        int x = st.top();
+        st.pop();
+        c += x * min(st.empty() ? INT_MAX : st.top(), a);
+        a = x;
         }
-        return dp[i][j] = mx;
-    }
-    
-    int mctFromLeafValues(vector<int>& arr) {
-     
-        int n = arr.size();
-        vector<vector<int>> dp(n + 1 , vector<int>(n + 1 , -1));
-        int ans = solve(0 , n - 1 , dp , arr);
-        return ans;
+        return c;   
     }
 };
