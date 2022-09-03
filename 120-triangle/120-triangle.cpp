b703fda8b6bdb754c6111e52069a024f889ddba4
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-09-03 23:45:45 +0530

    Time: 14 ms (26.13%), Space: 8.9 MB (52.45%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    
-    int res(vector<vector<int>> &t, int i, int j, int n, vector<vector<int>> &dp){
-        if(i >= n) return 0;
+    int n;
+    vector<vector<int>> dp;
+    int res(vector<vector<int>> &a, int i, int j){
+        if(i >= n or j >= n or j > i)
+            return 0;
         
+        if(i == n-1) return a[i][j];
         if(dp[i][j] != -1) return dp[i][j];
-        int sum = min(res(t, i+1, j, n, dp), res(t, i+1, j+1, n, dp)) + t[i][j];
-    
-        return dp[i][j] = sum;
+        
+        return dp[i][j] = min(res(a, i + 1, j), res(a, i + 1, j + 1)) + a[i][j];
     }
     
-    int minimumTotal(vector<vector<int>>& triangle) {
-        
-        int n = triangle.size();
-        vector<vector<int>> dp(n + 1, vector<int> (n + 1, -1));
-        
-        return res(triangle, 0, 0, n, dp);
+    int minimumTotal(vector<vector<int>>& a) {
+        n = a.size();
+        dp.resize(n, vector<int> (n, -1));
+        return res(a, 0, 0);
     }
 };
