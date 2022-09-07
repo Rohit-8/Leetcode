34e5333170bc5d9644afc827329bb2101e15d4e0
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-09-07 22:50:02 +0530

    Time: 3 ms (95.89%), Space: 12.9 MB (61.93%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -1,64 +1,57 @@
 class Solution {
 public:
     map<TreeNode*, TreeNode*> m;
-    
     void par(TreeNode* root){
+        if(root == NULL) return;
         queue<TreeNode*> q;
-        q.push(root); 
-        m[root] = NULL;
         
+        q.push(root);
         while(!q.empty()){
-            int n = q.size();
-            for(int i = 0; i < n; i++){
-                TreeNode* x = q.front();
-                q.pop();
-                if(x->left){
-                    m[x->left] = x;
-                    q.push(x->left);
-                }
-                if(x->right){
-                    m[x->right] = x;
-                    q.push(x->right);
-                }
+            TreeNode* t = q.front();
+            q.pop();
+            if(t -> left){
+                m[t -> left] = t;
+                q.push(t -> left);
+            }
+            if(t -> right){
+                m[t -> right] = t;
+                q.push(t -> right);
             }
         }
     }
-    
     vector<int> distanceK(TreeNode* root, TreeNode* t, int k) {
         vector<int> ans;
-        
-        if(k == 0){
-            ans.push_back(t->val);
-            return ans;
-        }
-        vector<int> vis(501, 0);
+        if(root == NULL) return ans;
         par(root);
+        
         int c = -1;
-        // cout<<m[root->left]->val;
+        unordered_map<TreeNode*, int> vis;
         queue<TreeNode*> q;
         q.push(t);
-        vis[t->val] = 1;
+        vis[t] = 1;
         
         while(!q.empty()){
-            
             int n = q.size();
             c++;
-            if(c > k) break;
+            
             for(int i = 0; i < n; i++){
-                TreeNode* x = q.front(); q.pop();
+                TreeNode* x = q.front();
+                q.pop();
+                if(c == k){
+                    // cout<<c <<" "<< x -> val<<" ";
+                    ans.push_back(x -> val);
+                    continue;
+                }
+                if(x -> left and vis[x -> left] <= 0)
+                    q.push(x -> left), vis[x -> left] = 1;
                 
-                if(c == k) ans.push_back(x->val);
+                if(x -> right and vis[x -> right] <= 0)
+                    q.push(x -> right), vis[x -> right] = 1;
                 
-                if(x->left and vis[x->left->val] == 0) 
-                    q.push(x->left);
-                if(x->right and vis[x->right->val] == 0)
-                    q.push(x->right);
-                // cout<<c;
-                if(m[x] and vis[m[x]->val] == 0)
-                    q.push(m[x]);
-                
-                vis[x->val] = 1;
+                if(x != root and vis[m[x]] <= 0)
+                    q.push(m[x]), vis[m[x]] = 1;
             }
+            if(c >= k) break;
         }
         
         return ans;
