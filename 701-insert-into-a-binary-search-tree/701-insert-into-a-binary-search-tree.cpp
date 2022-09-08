af29cbfc5888147f79003f7ca07846d32b7313c4
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-09-08 22:39:58 +0530

    Time: 178 ms (18.25%), Space: 56.7 MB (98.65%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
@@ -1,3 +1,32 @@
+class Solution {
+public:
+    TreeNode* res(TreeNode* root, int val){
+        if(root == NULL) return NULL;
+        
+        if(root -> left == NULL and root -> right == NULL)
+            return root;
+        if(root -> val > val and root -> left == NULL)
+            return root;
+        if(root -> val < val and root -> right == NULL)
+            return root;
+        if(root -> val >  val)
+            return res(root -> left , val);
+        
+        return res(root -> right, val);
+    }
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        TreeNode* t = new TreeNode(val);
+        if(root == NULL) return t;
+        
+        TreeNode* x = res(root, val);
+        
+        if(x -> val >  val)
+            x -> left = t;
+        else x -> right = t;
+        
+        return root;
+    }
+};
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -9,29 +38,3 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if(root == NULL) return new TreeNode(val);
-        TreeNode* t = root;
-        
-        while(t){
-            if(t->val < val){
-                if(t->right == NULL){
-                    t->right = new TreeNode(val);
-                    break;
-                }
-                else t = t->right;
-            }
-            else{
-                if(t->left == NULL){
-                    t->left = new TreeNode(val);
-                    break;
-                }
-                else t = t->left;
-            }
-        }
-        
-        return root;
-    }
-};
