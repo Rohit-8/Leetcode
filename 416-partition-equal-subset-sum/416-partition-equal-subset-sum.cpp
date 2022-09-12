21cf0b2acdc991ee05da8384cb94d666daf171ca
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-09-13 00:01:00 +0530

    Time: 1370 ms (5.02%), Space: 12.6 MB (63.11%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int n;
-    vector<vector<int>> dp;
-    bool res(vector<int> &a, int sum, int i){
-        if(i < 0) return false;
-        if(sum == 0) return true;
+    vector<vector<bool>> dp;
+//     bool res(vector<int> &a, int sum, int i){
+//         if(i < 0) return false;
+//         if(sum == 0) return true;
         
-        if(dp[i][sum] != -1) return dp[i][sum];
+//         if(dp[i][sum] != -1) return dp[i][sum];
         
-        bool f = res(a, sum, i - 1);
-        if(sum - a[i] >= 0)
-            f |= res(a, sum - a[i], i - 1);
+//         bool f = res(a, sum, i - 1);
+//         if(sum - a[i] >= 0)
+//             f |= res(a, sum - a[i], i - 1);
         
-        return dp[i][sum] = f;
-    }
+//         return dp[i][sum] = f;
+//     }
     
     bool canPartition(vector<int>& a) {
         int sum = 0;
@@ -22,7 +22,19 @@ public:
             sum += a[i];
         }
         if(sum % 2) return false;
-        dp.resize(n + 1, vector<int> (sum / 2 + 1, - 1));
-        return res(a, sum / 2, n - 1);
+        dp.resize(n + 1, vector<bool> (sum / 2 + 1, false));
+        int m = sum / 2;
+        for(int i = 0; i < n; i++){
+            dp[i][0] = true;
+        }
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j <= m; j++){
+                dp[i][j] = dp[i - 1][j];
+                if(a[i - 1] <= j)
+                    dp[i][j] = dp[i][j] | dp[i - 1][j - a[i - 1]];
+            }
+        }
+        
+        return dp[n - 1][m];
     }
 };
