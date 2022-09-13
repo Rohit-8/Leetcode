a4aca75be5f5531f72bbabf331936db4fabd008d
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-09-13 20:16:33 +0530

    Time: 75 ms (56.43%), Space: 28.8 MB (89.60%) - LeetHub

--- 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.cpp
+++ 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& v, int k) {
-        int i ;
-        
         int n = v.size() ;
-        
         int ans = 0 , prev = 0 ;
-        
-        for(i=1;i<n;i++)
-        {
+        for(int i=1;i<n;i++)
             v[i] = v[i] + v[i-1] ;
-        }
         
-        for(i=0;i<n;i++)
+        for(int i=0;i<n;i++)
         {
-            int curr = prev + k ;
+            int lower = lower_bound(v.begin()+i,v.end(),prev+k) - v.begin() ;
+            int upper = upper_bound(v.begin()+i,v.end(),prev+k) - v.begin() ;
             
-            int lower = lower_bound(v.begin()+i,v.end(),curr) - 
-                                        v.begin() ;
-            int upper = upper_bound(v.begin()+i,v.end(),curr) - 
-                                        v.begin() ;
-            
-            if(lower==n)
-            {
-                break ;
-            }
-            else
-            {
-                ans = ans + upper - lower ;
-            }
-            
+            if(lower==n) break ;
+            ans = ans + upper - lower ;
             prev = v[i] ;
         }
         
