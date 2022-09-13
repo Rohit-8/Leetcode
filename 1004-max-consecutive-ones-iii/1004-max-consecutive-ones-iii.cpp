5b1cd32e33614066635416df10a3086f5d99aea8
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-09-13 11:49:03 +0530

    Time: 78 ms (77.66%), Space: 55.5 MB (64.93%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -5,15 +5,14 @@ public:
         int c = 0, z = 0;
         for(int i = 0, j = 0; i < a.size(); i++){
             c++;
-            if(a[i] == 0) z++;
+            if(a[i] != 1) z++;
             if(z > k){
                 while(j <= i and z > k){
-                    if(a[j] == 0) z--;
+                    if(a[j] != 1) z--;
                     c--;
                     j++;
                 }
             }
-            
             ans = max(ans, c);
         }
         
