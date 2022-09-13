4e13bb4ad57d1575cf147e011a7bbeb0083868d9
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-09-13 19:31:15 +0530

    Time: 82 ms (8.62%), Space: 7 MB (51.49%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,25 +1,51 @@
 class Solution {
 public:
-    int characterReplacement(string a, int k) {
-        int ans = 0;
+    int characterReplacement(string s, int k) {
+        int low = 0 , high = 0 ;
         
-        for(char ch = 'A'; ch <= 'Z'; ch++){
-            int c = 0, z = 0;
-            for(int i = 0, j = 0; i < a.size(); i++){
-                c++;
-                if(a[i] != ch) z++;
-                if(z > k){
-                    while(j <= i and z > k){
-                        if(a[j] != ch) z--;
-                        c--;
-                        j++;
-                    }
+        int n = s.size() ;
+        
+        unordered_map<char,int> mp ;
+        
+        int ans = 1 ;
+        
+        while(high<n)
+        {
+            mp[s[high]]++ ;
+            
+            char ch = 'A' ;
+            int mx = mp['A'] ;
+            
+            int req = 0 ;
+            
+            int sum = 0 ;
+            
+            for(auto i : mp)
+            {
+                if(i.second>mx)
+                {
+                    mx = i.second ;
+                    ch = i.first ;
                 }
-
-                ans = max(ans, c);
+                
+                sum = sum + i.second ;
+            }
+            
+            req = sum - mx ;
+            
+            if(req<=k)
+            {
+                ans = max(ans,high-low+1) ;
+                high++ ;
+            }
+            else
+            {
+                mp[s[low]]-- ;
+                low++ ;
+                mp[s[high]]-- ;
             }
         }
         
-        return ans;
+        return ans ;
     }
 };
