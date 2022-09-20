36cb571d54b7e0830bf639dec1e642803b65aa27
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-09-20 22:10:48 +0530

    Time: 27 ms (41.89%), Space: 13.1 MB (82.14%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& mat) {
+    void setZeroes(vector<vector<int>>& a) {
+        int n = a.size();
+        int m = a[0].size();
         
-        bool f = false, f2 = false;
-        int n = mat.size(), m = mat[0].size();
-        for(int i = 0; i < n; i++)
+        int x = 0;
+        if(a[0][0] == 0) x = 1;
+        for(int i = 0; i < n; i++){
+            
             for(int j = 0; j < m; j++){
-                if(mat[i][j] == 0){
-                    if(i == 0) f2 = true;
-                    else if(j == 0) f = true;
-                    else mat[0][j] = mat[i][0] = 0;
+                if(a[i][j] == 0 and i > 0){
+                    a[0][j] = a[i][0] = 0;
+                }
+                else if(i == 0 and a[i][j] == 0) x = 1;
+            }
+        }
+        int f = 1;
+        if(x) f = 0;
+        for(int i = n - 1; i >= f; i--){
+            for(int j = m - 1; j >= 0; j --){
+                if(a[0][j] == 0 or a[i][0] == 0)
+                    a[i][j] = 0;
+                if(i == 0 and x){
+                    a[i][j] = 0;
                 }
             }
+        }
         
-        for(int i = n-1; i >= 0; i--)
-            for(int j = m-1; j >= 0; j--){
-                
-                if(mat[0][j] == 0 or mat[i][0] == 0)
-                    mat[i][j] = 0;
-                if(j == 0 and f) mat[i][j] = 0;
-                if(i == 0 and f2) mat[i][j] = 0;
-            }
         
-        if(f or f2) mat[0][0] = 0;
-        return;
+        return ;
     }
 };
