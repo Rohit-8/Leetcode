decc98743b82afdc2419be0bf93acfe1156f2386
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-09-22 11:53:11 +0530

    Time: 604 ms (34.65%), Space: 23.9 MB (38.11%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
     
-    bool ispal(string &s,int i, int j){
-        
-        while(i<j){
-            if(s[i]!=s[j])
-                return false;
-            
-            i++, j--;
-        }
-        return true;
+    int dp[2002][2002];
+    
+     bool isPalindrome(string &s ,int i,int j){
+        if(i>=j) return true;
+        if(s[i]!=s[j]) return false;
+        return isPalindrome(s,i+1,j-1);
     }
     
-    
-    int res(string &s,int i, int j, vector<vector<int>> &dp){
+    int pal(string &s,int i,int j){
         if(i>=j) return 0;
-        
-        if(ispal(s,i,j)) return dp[i][j] = 0;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int temp = INT_MAX;
-        for(int k = i; k < j; k++){
-    
-            if(ispal(s, i, k)){
-                int t = 1 + res(s,k+1,j, dp);
-                temp = min(t,temp);
-            }
+        int &ans=dp[i][j];
+        if(ans!=-1) return ans;
+        if(isPalindrome(s,i,j)){
+            return dp[i][j] = 0;
         }
-        
-        return dp[i][j] = temp;
+        int minn=INT_MAX;
+        for(int k=i;k<j;k++){
+            if(isPalindrome(s, i, k)){
+                int temp=pal(s,k+1,j)+1;
+          
+            minn=min(minn,temp);}
+        }
+        return dp[i][j] = minn;
     }
     
-    int minCut(string s) {
-        vector<vector<int>> dp(s.size()+1, vector<int> (s.size() + 1, -1));
-        
-        return res(s,0,s.size()-1, dp);
+    int minCut(string &s) {
+        memset(dp,-1,sizeof(dp));
+        int ans=0;
+        ans=pal(s,0,s.length()-1);
+        return ans;
     }
 };
