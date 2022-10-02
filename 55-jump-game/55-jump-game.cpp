efecf66e22e83f20a8f15532a137121900c0682f
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-10-02 22:34:07 +0530

    Time: 2952 ms (5.01%), Space: 51.4 MB (5.38%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    
-    int dp[100001];
-    int res(vector<int>& a, int n){
-        if(n == a.size()) return 1;
+    int n;
+    vector<int> v;
+    int res(vector<int> &a, int k){
+        if(k >= n - 1) return 1;
+        if(a[k] == 0) return 0;
         
-        if(a[n] == 0) return 0;
+        if(v[k] != -1) return v[k];
         
-        if(dp[n] != -1) return dp[n];
-        
-        int s = 0;
-        for(int i = 1; i <= a[n]; i++){
-            if(n + i + 1 >= a.size()) return dp[n] = 1;
-            if(res(a, n+i)) return dp[n] = 1;
+        int f = 0;
+        for(int i = 1; i <= a[k]; i++){
+            if(k + i >= n - 1) return v[k] = 1;
+            f |= res(a, k + i);
+            if(f) return v[k] = 1;
         }
         
-        return dp[n] = 0;
+        return v[k] = f;
     }
-    
     bool canJump(vector<int>& a) {
-        if(a.size()== 1) return true;
-        
-        memset(dp, -1, sizeof(dp));
-        
+        n = a.size();
+        v.resize(n + 1, -1);
         return res(a, 0);
     }
 };
