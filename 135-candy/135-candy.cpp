affdfafa9c46aaf0ac78b308598e05f2667c95c8
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-10-03 22:09:17 +0530

    Time: 57 ms (22.18%), Space: 17.7 MB (80.30%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    int candy(vector<int>& ratings) {
-        
-        vector<int> distribute(ratings.size(),1);
-        int candy=0;   
-        for(int i=0;i<ratings.size()-1;i++)
-        {
-            if(ratings[i+1]>ratings[i])
-            {
-                distribute[i+1]=distribute[i]+1;
-            } 
+    int candy(vector<int>& a) {
+        int n = a.size();
+        vector<int> v(n, 1);
+        for(int i = 1; i < n; i++){
+            if(a[i] > a[i - 1])
+                v[i] = v[i - 1] + 1;
         }
-        for(int i=ratings.size()-1;i>0;i--)
-        {
-            if(ratings[i-1]>ratings[i])
-            {
-                if(distribute[i-1]<=distribute[i])
-                    distribute[i-1]=distribute[i]+1;
+        for(int i = n - 2; i >= 0; i--){
+            if(a[i] > a[i + 1]){
+                v[i] = max(v[i], v[i + 1] + 1);
             }
         }
-        for(int i=0;i<ratings.size();i++)
-            candy = candy+distribute[i];
-
-        return candy;
-
+        
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            ans += v[i];
+        }
+        
+        return ans;
     }
 };
