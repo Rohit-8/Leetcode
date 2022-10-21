ab6e096f5b9dc2bd4dd5c3241f189c998c68b838
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-10-21 17:13:27 +0530

    Time: 628 ms (32.29%), Space: 148.7 MB (29.53%) - LeetHub

---
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    map<int, int> m;
+    int ans = -1;
+    int x = INT_MAX;
+    void res(vector<int> &a, int node, vector<int> &v, int c){
+        if(node == -1) return;
+        if(v[node]) return;
+        v[node] = 1;
+        
+        if(m.count(node)){
+            m[node] = max(m[node], c);
+            if(m[node] < x){
+                x = m[node];
+                cout<<x<<endl;
+                ans = node;
+            }
+            else if(m[node] == x and ans > -1){
+                ans = min(ans, node);
+            }
+        }
+        else m[node] = c;
+        c++;
+        
+        res(a, a[node], v, c);
+    }
+    
+    int closestMeetingNode(vector<int>& a, int node1, int node2) {
+        vector<int> v1(a.size(), 0), v2(a.size(), 0);
+        
+        int c = 0;
+        res(a, node1, v1, c);
+        c = 0;
+        res(a, node2, v2, c);
+        
+        return ans;
+    }
+};
