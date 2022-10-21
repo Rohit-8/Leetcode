4444a0072959a5a3d70c52ec354d8c35c4ee1101
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-10-21 17:39:15 +0530

    Time: 197 ms (90.41%), Space: 123.4 MB (43.40%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
-    unordered_map<int, int> m;
     int ans = -1;
-    int x = INT_MAX;
-    void res(vector<int> &a, int node, vector<int> &v, int c){
+    int mn = INT_MAX;
+    
+    void res(vector<int> &a, int node, vector<int> &vis, vector<int> &d, int c){
         if(node == -1) return;
-        if(v[node]) return;
-        v[node] = 1;
+        if(vis[node]) return;
+        vis[node] = 1;
         
-        if(m.count(node)){
-            m[node] = max(m[node], c);
-            if(m[node] < x){
-                x = m[node];
-                cout<<x<<endl;
+        if(d[node] > -1){
+            d[node] = max(d[node], c);
+            if(d[node] < mn){
+                mn = d[node];
                 ans = node;
             }
-            else if(m[node] == x and ans > -1){
+            else if(d[node] == mn and ans > -1){
                 ans = min(ans, node);
             }
         }
-        else m[node] = c;
+        else d[node] = c;
         c++;
         
-        res(a, a[node], v, c);
+        res(a, a[node], vis, d, c);
     }
     
     int closestMeetingNode(vector<int>& a, int node1, int node2) {
         vector<int> v1(a.size(), 0);
-        
+        vector<int> d(a.size(), -1);
         int c = 0;
-        res(a, node1, v1, c);
+        res(a, node1, v1, d, c);
         for(int i = 0; i < a.size(); i++){
             v1[i] = 0;
         }
         c = 0;
-        res(a, node2, v1, c);
+        res(a, node2, v1, d, c);
         
         return ans;
     }
