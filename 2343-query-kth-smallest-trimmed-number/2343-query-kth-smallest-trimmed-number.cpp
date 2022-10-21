cdc329509ae5de74cea55ff9d1ca2509a2b09ad5
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-10-21 23:54:27 +0530

    Time: 2281 ms (37.91%), Space: 389 MB (44.46%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
-    // struct myComp {
-    //      bool operator()(pair<string, int> const& a, pair<string, int> const& b)
-    //         const noexcept{
-    //         if(a.first != b.first)
-    //             return a.first < b.first;
-    //         return a.second < b.second;
-    //     }
-    // };
-    
     vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& q) {
         vector<int> ans;
         for(int k = 0; k < q.size(); k++){
             
             priority_queue<pair<string, int>> a;
             for(int i = 0; i < nums.size(); i++){
-                string s = nums[i].substr(nums[i].size() - q[k][1], q[k][1]);
+                
+                string s = nums[i].substr(nums[i].size() - q[k][1]);
                 if(a.size() < q[k][0])
                     a.push({s, i});
                 else if(a.top().first > s){
