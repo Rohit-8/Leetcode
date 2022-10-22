2633012b493b86e09be1866329658e24da72c271
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-10-22 14:14:35 +0530

    Time: 88 ms (27.69%), Space: 6.9 MB (22.22%) - LeetHub

--- 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
@@ -1,35 +1,27 @@
 #define ll long long
 class Solution {
 public:
-    ll mod = 1000000007;
+    ll MOD = 1000000007;
+    int delay,forget;
+    vector<ll> memo;
+    
+    long dp(int n) {
+        if(!n)
+            return 0;
+        if(memo[n]!=-1)
+            return memo[n];
+		
+        ll result=1;
+        for(int i=delay;i<forget;i++)
+            if(n-i>=0)
+                result=(result+dp(n-i))%MOD;
+        return memo[n]=result;
+    }
+    
     int peopleAwareOfSecret(int n, int delay, int forget) {
-        priority_queue<pair<ll, ll>, vector<pair<ll,ll>> , greater<pair<ll,ll>>> pq;
-        pq.push({1, 1});
-        
-        ll ans = 0;
-        for(int i = 2; i <= n; i++){
-            if(!pq.empty() and pq.top().first + forget == i){
-                pq.pop();
-            }
-            if(pq.empty()) break;
-            
-            vector<pair<ll, ll>> v;
-            ll c = 0;
-            while(!pq.empty() and pq.top().first + delay <= i){
-                v.push_back({pq.top().first, pq.top().second});
-                c += pq.top().second % mod;
-                pq.pop();
-            }
-            for(int j = 0; j < v.size(); j++){
-                pq.push(v[j]);
-            }
-            pq.push({i, c});
-        }
-        while(!pq.empty()){
-            ans += (pq.top().second % mod);
-            pq.pop();
-        }
-        
-        return ans % mod;
+        this->delay=delay;
+        this->forget=forget;
+        memo.resize(n+1,-1);
+        return (dp(n)-dp(n-forget)+MOD)%MOD;
     }
 };
