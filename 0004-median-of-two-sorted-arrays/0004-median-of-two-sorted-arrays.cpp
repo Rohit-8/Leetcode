8a656a4c2ff3b266f16a43ccf7c958f5d075e14e
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-10-30 19:11:14 +0530

    Time: 67 ms (63.71%), Space: 89.4 MB (66.70%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -14,25 +14,22 @@ public:
             int cut1 = (r+l)>>1;
             int cut2 = (n+m+1) / 2 - cut1;
             
-            int left1 = cut1 == 0? INT_MIN : a[cut1-1];
-            int left2 = cut2 == 0? INT_MIN : b[cut2-1];
+            int left1 = cut1 == 0 ? INT_MIN : a[cut1 - 1];
+            int left2 = cut2 == 0 ? INT_MIN : b[cut2 - 1];
 
-            int right1 = cut1 == n? INT_MAX : a[cut1];
-            int right2 = cut2 == m? INT_MAX : b[cut2];
+            int right1 = cut1 == n ? INT_MAX : a[cut1];
+            int right2 = cut2 == m ? INT_MAX : b[cut2];
             
             if(left1 <= right2 and left2 <= right1){
                 if((n+m) % 2 == 0) 
                     return (max(left1, left2) + min(right1,right2))/2.0;
                 
-                return max(left1,left2);
-                
+                return max(left1,left2); 
             }
             else if(left1 > right2) r = cut1 - 1;
             else l = cut1 + 1;
-
         }
         
         return 0.0;
-        
     }
 };
