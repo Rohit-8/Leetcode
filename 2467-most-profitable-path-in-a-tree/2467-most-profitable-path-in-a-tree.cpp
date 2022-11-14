aa3341abfca13bf20ce2cb5a157f0cceda36adaa
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-11-14 14:48:49 +0530

    Time: 1180 ms (33.33%), Space: 170.1 MB (83.33%) - LeetHub

---
+++ 2467-most-profitable-path-in-a-tree/2467-most-profitable-path-in-a-tree.cpp
@@ -0,0 +1,88 @@
+class Solution {
+public:
+    unordered_map<int, int> path;
+    long long dfs(vector<vector<int>> &adj, vector<int> &a, vector<int> &vis, int node, int x){
+        if(vis[node]) return 0;
+        long long sum = 0;
+        if(x == node){
+            sum = a[node] / 2;
+            a[node] = 0; 
+            // cout<<c<<endl;
+        }
+        else {
+            // a[x] = 0;
+            sum = a[node];
+            a[node] = 0;
+        }
+        
+        vis[node] = 1;
+        long long d = INT_MIN;
+        bool f = false;
+        for(auto c: adj[node]){
+            if(vis[c] == 0){
+                d =  max(d,dfs(adj, a, vis, c, path[x]));
+                f = true;
+            }
+        }
+        if(f)
+            sum += d;
+        // a[x] = 0;
+        return sum;
+    }
+    
+    int mostProfitablePath(vector<vector<int>>& a, int bob, vector<int>& am) {
+        
+        vector<vector<int>> adj(a.size() + 1);
+        for(int i = 0; i < a.size(); i++){
+            adj[a[i][0]].push_back(a[i][1]);
+            adj[a[i][1]].push_back(a[i][0]);
+        }
+        
+        queue<int> q;
+        q.push(0);
+        path[0] = 0;
+        
+        while(!q.empty()){
+            int n = q.size();
+            
+            bool f = false;
+            for(int i = 0; i < n; i++){
+                int c = q.front();
+                q.pop();
+                for(auto x: adj[c]){
+                    if(path.count(x) == false){
+                        path[x] = c;
+                        q.push(x);
+                    }
+                    if(x == bob){
+                        f = true;
+                        break;
+                    }
+                }
+                if(f) break;
+            }
+            
+            if(f) break;
+        }
+        int c = 0;
+        int x = bob;
+        while(1){
+            cout<<x<<" ";
+            c++;
+            if(x == 0) break;
+            x = path[x];
+            
+        }
+        cout<<endl<< c<<endl;
+        x = c / 2;
+        c = bob;
+        while(x--){
+            cout<<c<<" "<< am[c]<<endl;
+            am[c] = 0;
+            c = path[c];
+        }
+        vector<int> vis(a.size() + 1, 0);
+        
+        return dfs(adj, am, vis, 0, bob);
+    }
+};
