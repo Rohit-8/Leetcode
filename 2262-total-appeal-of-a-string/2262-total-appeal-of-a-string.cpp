6b2f1d60894e5aa2d918ab4bd93b7ffce81f93f9
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-11-15 22:25:39 +0530

    Time: 172 ms (10.94%), Space: 10.6 MB (77.64%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
@@ -6,7 +6,6 @@ public:
         map<char, int> mp;
         for(long long i = 0; i < n; i++){
             if(mp.count(s[i])){
-                // ans += (i - mp[s[i]] - 1);
                 long long x = n - i;
                 x *= (i - mp[s[i]]);
                 ans += x;
@@ -18,7 +17,6 @@ public:
                 ans += x;
                 mp[s[i]] = i;
             }
-            // cout<<i<<" "<<x <<endl;
         }
         
         return ans;
