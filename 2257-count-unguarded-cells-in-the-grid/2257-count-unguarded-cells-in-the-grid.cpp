52b913463f493eb42861198c5807c975f83a6929
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-11-17 14:43:22 +0530

    Time: 1144 ms (50.19%), Space: 153.9 MB (76.00%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
@@ -1,25 +1,8 @@
 class Solution {
 public:
-//     void res(vector<vector<int>> &v, ){
-        
-        
-        
-//     }
-    
-    int countUnguarded(int n, int m, vector<vector<int>>& g, vector<vector<int>>& w) {
-        vector<vector<int>> v(n, vector<int> (m, 0));
-        for(int i = 0; i < w.size(); i++){
-            v[w[i][0]][w[i][1]] = 2;
-        }
-        
-        for(int i = 0; i < g.size(); i++){
-            v[g[i][0]][g[i][1]] = 2;
-        }
-        
-        for(int i = 0; i < g.size(); i++){
-            // res(v, g[i][0], g[i][1]);
-            int k = g[i][0], l = g[i][1];
-            for(int i = k - 1; i >= 0 and v[i][l] != 2 and v[i][l] != 3; i--){
+    void res(vector<vector<int>> &v, int k, int l){
+        // int j = l;
+        for(int i = k - 1; i >= 0 and v[i][l] != 2 and v[i][l] != 3; i--){
             // if(v[i][l] == 0)
             v[i][l] = 3;
         }
@@ -36,6 +19,21 @@ public:
             // if(v[k][j] == 0)
             v[k][j] = 4;
         }
+        // v[k][l] = 2;
+    }
+    
+    int countUnguarded(int n, int m, vector<vector<int>>& g, vector<vector<int>>& w) {
+        vector<vector<int>> v(n, vector<int> (m, 0));
+        for(int i = 0; i < w.size(); i++){
+            v[w[i][0]][w[i][1]] = 2;
+        }
+        
+        for(int i = 0; i < g.size(); i++){
+            v[g[i][0]][g[i][1]] = 2;
+        }
+        
+        for(int i = 0; i < g.size(); i++){
+            res(v, g[i][0], g[i][1]);
         }
         
         int ans = 0;
