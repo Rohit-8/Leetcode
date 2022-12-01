b4e60c805cd942d15710b25bc1c20e92317cc973
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-12-01 19:03:27 +0530

    Time: 2585 ms (5.34%), Space: 254.8 MB (18.70%) - LeetHub

--- 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
+++ 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
@@ -1,76 +1,43 @@
 class Solution {
 public:
-    int maxtilescovered(int idx , int nc , string &s , int len , vector<int>& v , vector<vector<int>>& dp)
-    {
-        while(idx<s.size()&&s[idx]=='0')
-        {
-            idx++ ;
+    // int ans = INT_MAX;
+    int res(string &s, int n, int nc, int cl, vector<vector<int>> &dp){
+        if(n >= s.size() or nc <= 0){
+            // int c = 0;
+            // for(int i = 0; i < s.size(); i++){
+            //     if(s[i] == '1') c++;
+            // }
+            // ans = min(ans, c);
+            return 0;
         }
+        if(dp[n][nc] != -1)
+            return dp[n][nc];
         
-        if(nc==0||idx>=s.size())
-        {
-            return 0 ;
+        int l = res(s, n + 1, nc, cl, dp);
+        
+        if(s[n] == '1'){
+            int c = 0;
+            for(int i = n; i < s.size() and i < n + cl; i++)
+                if(s[i] == '1'){
+                    // s[i] = '0';
+                    c ++;
+                }
+            
+            l = max(l, c + res(s, n + cl, nc - 1, cl, dp));
+            // for(int i = n; i < s.size() and i < n + cl; i++)
+            //     s[i] = '1';
         }
-        
-        if(dp[idx][nc]!=-1)
-        {
-            return dp[idx][nc] ;
-        }
-        
-        int i ;
-        
-        int count = 0 ;
-        
-        int low = idx , high = min(idx+len-1,(int)s.size()-1) ;
-        
-        count = v[high] ;
-        
-        if(low-1>=0)
-        {
-            count = count - v[low-1] ;
-        }
-        
-        int pick = count + maxtilescovered(idx+len,nc-1,s,len,v,dp) ;
-        
-        int notpick = maxtilescovered(idx+1,nc,s,len,v,dp) ;
-        
-        return dp[idx][nc] = max(pick,notpick) ;
+        return dp[n][nc] = l;
     }
     
-    int minimumWhiteTiles(string &s, int nc, int len) {
-        int n = s.size() ;
-        
-        int i ;
-        
-        vector<int> v(n,0) ;
-        
-        for(i=0;i<s.size();i++)
-        {
-            if(s[i]=='1')
-            {
-                v[i]++ ;
-            }
+    int minimumWhiteTiles(string s, int nc, int cl) {
+        vector<vector<int>> dp(s.size() + 1, vector<int> (nc + 1, -1));
+        int c = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '1') c++;
         }
+        return c - res(s, 0, nc, cl, dp);
         
-        for(i=1;i<v.size();i++)
-        {
-            v[i] = v[i] + v[i-1] ;
-        }
-        
-        vector<vector<int>> dp(s.size()+1,vector<int>(nc+1,-1)) ;
-        
-        int curr = maxtilescovered(0,nc,s,len,v,dp) ;
-        
-        int count = 0 ;
-        
-        for(i=0;i<s.size();i++)
-        {
-            if(s[i]=='0')
-            {
-                count++ ;
-            }
-        }
-        
-        return s.size() - (count+curr) ;
+        // return ans;
     }
 };
