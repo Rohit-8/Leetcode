5dea129881637c67f0f7aac5c8ffbb40fc42b80d
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-12-12 17:24:12 +0530

    Time: 228 ms (28.57%), Space: 109.1 MB (7.14%) - LeetHub

--- 2499-minimum-total-cost-to-make-arrays-unequal/2499-minimum-total-cost-to-make-arrays-unequal.cpp
+++ 2499-minimum-total-cost-to-make-arrays-unequal/2499-minimum-total-cost-to-make-arrays-unequal.cpp
@@ -16,7 +16,6 @@ public:
             if(ar[i] == ele){
                 count++;
             }
-            // else count --;
         }
         if(count > size / 2)
             return {ele, count};
@@ -26,10 +25,6 @@ public:
     
     long long minimumTotalCost(vector<int>& a, vector<int>& b) {
         int n = a.size();
-        // int x = majorityElement(a, n);
-        // int y = majorityElement(b, n);
-        // if((x != -1) and (x == y))
-        //     return -1;
         
         vector<int> v;
         long long ans = 0;
@@ -37,11 +32,11 @@ public:
             if(a[i] == b[i])
                 v.push_back(a[i]), ans += i * 1ll;
         }
-        cout << " ans " << ans << endl;
-        for(int i = 0; i < v.size(); i++){
-            cout << v[i] <<" ";
-        }
-        cout << endl;
+        // cout << " ans " << ans << endl;
+        // for(int i = 0; i < v.size(); i++){
+        //     cout << v[i] <<" ";
+        // }
+        // cout << endl;
         
         pair<int, int> t = majorityElement(v, (int)v.size());
         int el = t.first, c = t.second;
@@ -50,8 +45,9 @@ public:
         }
         c = c - (v.size() - c);
         
-        cout << " el " << el << endl;
-        cout << " c "<< c <<endl;
+        // cout << " el " << el << endl;
+        // cout << " c "<< c <<endl;
+        
         for(int i = 0; i < n; i++){
             if(a[i] != el and b[i] != el and a[i] != b[i]){
                 ans += i;
