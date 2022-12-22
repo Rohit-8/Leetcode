cffd9e7c8176ba8b50d7e2dc0e1e3a7fc95a161c
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-12-22 12:05:57 +0530

    Time: 9 ms (30.94%), Space: 6.9 MB (89.40%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int myAtoi(string &s) {
+        if(s.size() == 0) return 0;
+        int i = 0;
+        while(i < s.size() and s[i] == ' ') i++;
+        
+        long mul = 1;
+        if(s[i] == '-') mul = -1, i++;
+        else if(s[i] == '+') i++;
+        if(i >= s.size()) return 0;
+        
+        long ans = 0;
+        for(; i < s.size(); i++){
+            if(s[i] < '0' or s[i] > '9') break;
+            ans = ans * 10 + (s[i] - '0');
+            if(ans >= INT_MAX and mul == 1){
+                cout<<ans;
+                return INT_MAX;
+            }
+            else if(ans > INT_MAX and mul == -1){
+                return INT_MIN;
+            }
+        }
+        
+        return ans * mul;
+    }
+};
