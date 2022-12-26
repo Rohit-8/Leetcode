e9451ff866e0661979ca9393619169b51f989c56
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-12-26 22:41:57 +0530

    Time: 271 ms (89.15%), Space: 121.3 MB (48.02%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,35 +1,49 @@
 class Solution {
 public:
-    ListNode* reverse(ListNode* head){
-        ListNode* t = head;
-        ListNode* r = NULL;
-        while(t and t -> next){
-            ListNode* temp = t -> next;
-            t -> next = r;
-            r = t;
-            t = temp;
-        }
-        t -> next = r;
-        return t;
+    // ListNode* reverse(ListNode* head){
+    //     ListNode* t = head;
+    //     ListNode* r = NULL;
+    //     while(t and t -> next){
+    //         ListNode* temp = t -> next;
+    //         t -> next = r;
+    //         r = t;
+    //         t = temp;
+    //     }
+    //     t -> next = r;
+    //     return t;
+    // }
+    int ans = 0;
+    ListNode* ptr = NULL;
+    int pairSum(ListNode* head) {
+//         ListNode* slow = head, *fast = head -> next;
+//         while(fast and fast -> next){
+//             slow = slow -> next;
+//             fast = fast -> next -> next;
+//         }
+//         fast = reverse(slow -> next);
+//         slow -> next = NULL;
+//         int ans = 0;
+        
+//         while(fast){
+//             ans = max(ans, fast -> val + head -> val);
+//             fast = fast -> next;
+//             head = head -> next;
+//         }
+        
+//         return ans;
+        
+        // 2nd method using recursion //////
+        ptr = head;
+        res(head);
+        return ans;
     }
     
-    int pairSum(ListNode* head) {
-        ListNode* slow = head, *fast = head -> next;
-        while(fast and fast -> next){
-            slow = slow -> next;
-            fast = fast -> next -> next;
-        }
-        fast = reverse(slow -> next);
-        slow -> next = NULL;
-        int ans = 0;
+    void res(ListNode* root){
+        if(root == NULL or ptr == NULL) return;
         
-        while(fast){
-            ans = max(ans, fast -> val + head -> val);
-            fast = fast -> next;
-            head = head -> next;
-        }
-        
-        return ans;
+        res(root -> next);
+        ans = max(ans, root -> val + ptr -> val);
+        ptr = ptr -> next;
     }
 };
 /**
