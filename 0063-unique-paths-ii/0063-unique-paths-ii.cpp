382c1de3b770e12cab26e6eb9f8784947ee03bf9
Rohit Raj Yadav <76719166+Rohit-8@users.noreply.github.com>
2022-12-31 12:15:08 +0530

    Time: 4 ms (67.38%), Space: 7.9 MB (48.56%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -9,9 +9,37 @@ public:
             return dp[i][j];
         return dp[i][j] = helper(a, i + 1, j, dp) + helper(a, i, j + 1, dp);
     }
+    
     int uniquePathsWithObstacles(vector<vector<int>>& a) {
-        vector<vector<int>> dp(a.size() + 1, vector<int> (a[0].size() + 1, -1));
+        // vector<vector<int>> dp(a.size() + 1, vector<int> (a[0].size() + 1, -1));
         
-        return helper(a, 0, 0, dp);
+        // return helper(a, 0, 0, dp);
+        ///////////// method 2 ///////////////
+        int n = a.size(), m = a[0].size();
+        if(a[n - 1][m - 1] or a[0][0])
+            return 0;
+        vector<vector<int>> dp(a.size() + 1, vector<int> (a[0].size() + 1, 0));
+        dp[0][0] = 1;
+        for(int i = 1; i < m; i++){
+            if(a[0][i] == 1)
+                dp[0][i] = 0;
+            else dp[0][i] = dp[0][i - 1];
+        }
+        for(int i = 1; i < n; i++){
+            if(a[i][0] == 1)
+                dp[i][0] = 0;
+            else dp[i][0] = dp[i - 1][0];
+        }
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(a[i][j] == 1){
+                    dp[i][j] = 0;
+                }
+                else dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        
+        return dp[n - 1][m - 1];
     }
 };
